97229cadff27a0148e69609445a69a7a964e5ba1
mJota666 <micode2744@gmail.com>
2023-07-28 10:27:41 +0700

    minh 98%complete

--- minhtext/newtest/text.py
+++ minhtext/newtest/text.py
@@ -5,48 +5,44 @@ import time
 import threading
 import argparse
 
-
-
 # Function to read the config file manually
 def read_config(config_file):
-    config = {
-        'CACHE_DIR': '',
-        'ACCESS_LIMIT': 0,
-        'ACCESS_PERIOD': 0,
-        'SERVER_IP': '',
-        'SERVER_PORT': 0,
-        'CACHE_TIME': 0,
-        'WHITELISTING': [],
-        'TIME_START': 0,
-        'TIME_END': 0
-    }
+    CACHE_DIR = None
+    ACCESS_LIMIT = None
+    SERVER_IP = None
+    SERVER_PORT = None
+    CACHE_TIME = None
+    WHITELISTING = []
+    time_range = None
+
     with open(config_file, 'r') as f:
         for line in f:
-            key, value = line.strip().split('=')
-            key = key.strip()
-            value = value.strip()
+            if 'CACHE_DIR' in line:
+                CACHE_DIR = line.split('=')[1].strip()
+                if not os.path.exists(CACHE_DIR):
+                    os.makedirs(CACHE_DIR)
+            elif 'ACCESS_LIMIT' in line:
+                ACCESS_LIMIT = int(line.split('=')[1].strip())
+            elif 'SERVER_IP' in line:
+                SERVER_IP = line.split('=')[1].strip()
+            elif 'SERVER_PORT' in line:
+                SERVER_PORT = int(line.split('=')[1].strip())
+            elif 'CACHE_TIME' in line:
+                CACHE_TIME = int(line.split('=')[1].strip())
+            elif 'WHITELISTING' in line:
+                WHITELISTING = [domain.strip() for domain in line.split('=')[1].split(',')]
+            elif 'TIME' in line:
+                time_range = line.split('=')[1].strip()
 
-            if key == 'CACHE_DIR':
-                config['CACHE_DIR'] = value
-            elif key == 'ACCESS_LIMIT':
-                config['ACCESS_LIMIT'] = int(value)
-            elif key == 'ACCESS_PERIOD':
-                config['ACCESS_PERIOD'] = int(value)
-            elif key == 'SERVER_IP':
-                config['SERVER_IP'] = value
-            elif key == 'SERVER_PORT':
-                config['SERVER_PORT'] = int(value)
-            elif key == 'CACHE_TIME':
-                config['CACHE_TIME'] = int(value)
-            elif key == 'WHITELISTING':
-                config['WHITELISTING'] = [domain.strip() for domain in value.split(',')]
-            elif key == 'TIME':
-                time_range = value.split('-')
-                config['TIME_START'] = int(time_range[0].strip())
-                config['TIME_END'] = int(time_range[1].strip())
-    if not all(config.values()):
-        raise ValueError("Invalid config file format. Make sure all configuration values are specified.")
-    return config
+    if any(val is None for val in [CACHE_DIR, ACCESS_LIMIT, SERVER_IP, SERVER_PORT, CACHE_TIME, time_range]):
+        raise ValueError("Invalid config file format. Make sure all required parameters are specified.")
+    
+    # Split the time range into start and end times
+    start_time_str, end_time_str = time_range.split('-')
+    start_time = time.strptime(start_time_str, "%H:%M:%S")
+    end_time = time.strptime(end_time_str, "%H:%M:%S")
+
+    return CACHE_DIR, ACCESS_LIMIT, SERVER_IP, SERVER_PORT, CACHE_TIME, WHITELISTING, start_time, end_time
 
 # Kiểm tra xem domain có nằm trong whitelist không
 def is_whitelisted(domain, whitelist):
@@ -56,24 +52,57 @@ def is_whitelisted(domain, whitelist):
     return False
 
 # Kiểm tra giới hạn truy cập theo thời gian
-def check_access_limit(start_time, end_time):
-    current_hour = time.localtime().tm_hour
-    return start_time <= current_hour < end_time
+def check_ACCESS_LIMIT(start_time, end_time):
+    current_time = time.localtime()
+    CURRENT_TIME = current_time.tm_hour * 3600 + current_time.tm_min * 60 + current_time.tm_sec
+    START_TIME = start_time.tm_hour * 3600 + start_time.tm_min * 60 + start_time.tm_sec
+    END_TIME = end_time.tm_hour * 3600 + end_time.tm_min * 60 + end_time.tm_sec
+    return START_TIME <= CURRENT_TIME <= END_TIME
 
-# Hàm lưu ảnh vào cache
+# Kiểm tra xem cache đã hết hạn chưa
+def is_cache_expired(filename, CACHE_TIME):
+    current_time = time.time()
+    file_mtime = os.path.getmtime(filename)
+    cache_age = current_time - file_mtime
+    return cache_age > CACHE_TIME
+
+# Hàm lưu ảnh vào cache và ghi lại thời điểm lưu cache
 def save_image_to_cache(url, data, CACHE_DIR):
     filename = os.path.join(CACHE_DIR, url.replace('/', '_').replace(':', '_'))
     with open(filename, 'wb') as f:
         f.write(data)
+    with open(filename + '.time', 'w') as f_time:
+        f_time.write(str(time.time()))
 
-# Hàm lấy dữ liệu ảnh từ cache
-def get_image_from_cache(url, CACHE_DIR):
+# Hàm lấy dữ liệu ảnh từ cache và kiểm tra thời gian cache
+def get_image_from_cache(url, CACHE_DIR, CACHE_TIME):
     filename = os.path.join(CACHE_DIR, url.replace('/', '_').replace(':', '_'))
-    if os.path.exists(filename):
-        with open(filename, 'rb') as f:
-            return f.read()
+    cache_time_filename = filename + '.time'
+    if os.path.exists(filename) and os.path.exists(cache_time_filename):
+        if not is_cache_expired(cache_time_filename, CACHE_TIME):
+            with open(filename, 'rb') as f:
+                return f.read()
+        else:
+            # Xóa cache và cache time nếu đã hết hạn
+            os.remove(filename)
+            os.remove(cache_time_filename)
     return None
-
+# Luồng thực hiện việc loại bỏ các đối tượng đã hết hạn khỏi cache sau mỗi 15 phút
+def remove_expired_cache(CACHE_DIR, CACHE_TIME):
+    while True:
+        time.sleep(CACHE_TIME) 
+        current_time = time.time()
+        for filename in os.listdir(CACHE_DIR):
+            if filename.endswith('.time'):
+                cache_time_filename = os.path.join(CACHE_DIR, filename)
+                with open(cache_time_filename, 'r') as f_time:
+                    cache_time = float(f_time.read())
+                cache_age = current_time - cache_time
+                if cache_age > CACHE_TIME:
+                    # Xóa cache và cache time nếu đã hết hạn
+                    cache_filename = os.path.join(CACHE_DIR, filename[:-5])
+                    os.remove(cache_filename)
+                    os.remove(cache_time_filename)
 # Biến lưu trữ logs truy cập
 access_logs = {}
 
@@ -82,7 +111,7 @@ def is_image(url):
     return any(url.endswith(ext) for ext in ['.jpg', '.jpeg', '.png', '.gif', '.bmp'])
 
 def proxy_thread(client_socket, config):
-    CACHE_DIR, ACCESS_LIMIT, SERVER_IP, SERVER_PORT, CACHE_TIME, WHITELISTING, TIME_START, TIME_END = config
+    CACHE_DIR, ACCESS_LIMIT, SERVER_IP, SERVER_PORT, CACHE_TIME, WHITELISTING, START_TIME, END_TIME = config
     request_data = client_socket.recv(4096)
     request_string = request_data.decode('utf-8')
 
@@ -105,8 +134,10 @@ def proxy_thread(client_socket, config):
         return
 
     # Kiểm tra giới hạn truy cập theo thời gian
-    if not check_access_limit(TIME_START, TIME_END):
-        response = 'HTTP/1.1 403 Forbidden\r\n\r\nAccess is limited from {} to {}'.format(TIME_START, TIME_END)
+    if not check_ACCESS_LIMIT(START_TIME, END_TIME):
+        start_time_str = time.strftime('%H:%M:%S', START_TIME)
+        end_time_str = time.strftime('%H:%M:%S', END_TIME)
+        response = 'HTTP/1.1 403 Forbidden\r\n\r\nAccess is limited from {} to {}'.format(start_time_str, end_time_str)
         client_socket.sendall(response.encode('utf-8'))
         client_socket.close()
         return
@@ -118,7 +149,7 @@ def proxy_thread(client_socket, config):
         return
 
     if is_image(url):
-        cached_data = get_image_from_cache(url)
+        cached_data = get_image_from_cache(url, CACHE_DIR, CACHE_TIME)
         if cached_data:
             response = 'HTTP/1.1 200 OK\r\n\r\n'
             client_socket.sendall(response.encode('utf-8'))
@@ -132,7 +163,7 @@ def proxy_thread(client_socket, config):
     response = requests.request(method, f"http://{url}", headers=headers, stream=True)
 
     if is_image(url) and response.status_code == 200:
-        save_image_to_cache(url, response.content)
+        save_image_to_cache(url, response.content, CACHE_DIR)
 
     for key, value in response.headers.items():
         client_socket.send(f"{key}: {value}\r\n".encode('utf-8'))
@@ -147,14 +178,20 @@ def proxy_thread(client_socket, config):
 
 def main(config_file):
     config = read_config(config_file)
-    CACHE_DIR, ACCESS_LIMIT, SERVER_IP, SERVER_PORT, CACHE_TIME, WHITELISTING, TIME_START, TIME_END  = config
+    CACHE_DIR, ACCESS_LIMIT, SERVER_IP, SERVER_PORT, CACHE_TIME, WHITELISTING, START_TIME, END_TIME = config
     if not os.path.exists(CACHE_DIR):
         os.makedirs(CACHE_DIR)
+
+    # Bắt đầu luồng loại bỏ cache hết hạn
+    remove_cache_thread = threading.Thread(target=remove_expired_cache, args=(CACHE_DIR, CACHE_TIME))
+    remove_cache_thread.daemon = True
+    remove_cache_thread.start()
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((SERVER_IP, SERVER_PORT))
     server.listen(ACCESS_LIMIT)
 
-    print('Proxy server is listening on port 8888...')
+    print('Proxy server ' + SERVER_IP + ' is listening on port ' + str(SERVER_PORT) + '...')
     
     while True:
         client_socket, addr = server.accept()
@@ -166,6 +203,5 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Proxy Server with Config File')
     parser.add_argument('config', help='Path to the config file')
     args = parser.parse_args()
-
     main(args.config)
 
