0a54736e122519804d01e7f547377d23f5073e4b
Võ Thành Nghĩa <63101593+voasd00@users.noreply.github.com>
2023-08-11 10:01:58 +0700

    delete clone folder and add newest code to main folder

--- minhtext/newtest/run.bat
+++
@@ -1,1 +0,0 @@
-python test.py newconfig.txt
--- proxy.py
+++ proxy.py
@@ -1,183 +1,323 @@
-from socket import *
-import sys
-import threading
 import os
+import socket
 import time
-from datetime import datetime
+import threading
+import argparse
+import shutil
 
-# Whitelisted websites
-whitelist = ["example.com", "example2.com"]  # Add allowed websites here
+# Kiểm tra code gửi về có nằm trong danh sách cấm hay không
+error_codes = [b'400', b'401', b'403', b'404', b'405', b'408', b'502', b'503']
 
-# Cache directory for storing images
-cache_directory = "cache"
-if not os.path.exists(cache_directory):
-    os.makedirs(cache_directory)
+# Function to read the config file manually
+def read_config(config_file):
+    CACHE_DIR = None
+    ACCESS_LIMIT = None
+    SERVER_IP = None
+    SERVER_PORT = None
+    CACHE_TIME = None
+    WHITELISTING = []
+    time_range = None
 
-# Set caching time for images (in seconds)
-cache_time = 900  # 15 minutes
+    with open(config_file, 'r') as f:
+        for line in f:
+            if 'CACHE_DIR' in line:
+                CACHE_DIR = line.split('=')[1].strip()
+                if not os.path.exists(CACHE_DIR):
+                    os.makedirs(CACHE_DIR)
+            elif 'ACCESS_LIMIT' in line:
+                ACCESS_LIMIT = int(line.split('=')[1].strip())
+            elif 'SERVER_IP' in line:
+                SERVER_IP = line.split('=')[1].strip()
+            elif 'SERVER_PORT' in line:
+                SERVER_PORT = int(line.split('=')[1].strip())
+            elif 'CACHE_TIME' in line:
+                CACHE_TIME = int(line.split('=')[1].strip())
+            elif 'WHITELISTING' in line:
+                WHITELISTING = [domain.strip() for domain in line.split('=')[1].split(',')]
+            elif 'TIME' in line:
+                time_range = line.split('=')[1].strip()
 
-# Dictionary to track the last access time of websites
-last_access_time = {}
+    if any(val is None for val in [CACHE_DIR, ACCESS_LIMIT, SERVER_IP, SERVER_PORT, CACHE_TIME, time_range]):
+        raise ValueError("Invalid config file format. Make sure all required parameters are specified.")
+    
+    # Split the time range into start and end times
+    start_time_str, end_time_str = time_range.split('-')
+    start_time = time.strptime(start_time_str, "%H:%M:%S")
+    end_time = time.strptime(end_time_str, "%H:%M:%S")
 
-# Read config file to get server IP and port
-def read_config_file(filename):
-    with open(filename, "r") as file:
-        config_data = file.read().splitlines()
-        server_ip = config_data[0].split("=")[1].strip()
-        server_port = int(config_data[1].split("=")[1].strip())
-        return server_ip, server_port
+    return CACHE_DIR, ACCESS_LIMIT, SERVER_IP, SERVER_PORT, CACHE_TIME, WHITELISTING, start_time, end_time
 
-# Function to check if a website is whitelisted
-def is_whitelisted(host):
-    return any(host.endswith(allowed_host) for allowed_host in whitelist)
+# Kiểm tra https
+def is_https(request_string):
+    if ("CONNECT" in request_string):
+        return True
+    return False
 
-def read_response403(filename):
-    if filename == "custom403.html":
-        with open("custom403.html", "rb") as html_file:
-            content = html_file.read()
-            response = b"HTTP/1.0 403 Forbidden\r\nContent-Type: text/html\r\n\r\n" + content
+# Kiểm tra xem domain có nằm trong whitelist không
+def is_whitelisted(domain, whitelist):
+    for i in whitelist:
+        if i in domain:
+            return True
+    return False
+
+def response403():
+    with open("custom403.html", "rb") as html_file:
+        content = html_file.read()
+        response = b"HTTP/1.0 403 Forbidden\r\nContent-Type: text/html\r\n\r\n" + content
     return response
 
-# Function to check if the current time is within the allowed hours (8 AM to 8 PM)
-def is_within_allowed_time():
-    now = datetime.now().time()
-    return now >= datetime.strptime("08:00:00", "%H:%M:%S").time() and now <= datetime.strptime("20:00:00", "%H:%M:%S").time()
+# Kiểm tra giới hạn truy cập theo thời gian
+def check_ACCESS_LIMIT(start_time, end_time):
+    current_time = time.localtime()
+    CURRENT_TIME = current_time.tm_hour * 3600 + current_time.tm_min * 60 + current_time.tm_sec
+    START_TIME = start_time.tm_hour * 3600 + start_time.tm_min * 60 + start_time.tm_sec
+    END_TIME = end_time.tm_hour * 3600 + end_time.tm_min * 60 + end_time.tm_sec
+    return START_TIME <= CURRENT_TIME <= END_TIME
 
-# Function to handle concurrent client connections
-def handle_client(tcpCliSock):
-    # Receive the HTTP request from the client
-    message = tcpCliSock.recv(1024).decode()
-    print("Received request:\n", message)
+# Kiểm tra xem cache đã hết hạn chưa
+def is_cache_expired(filename, CACHE_TIME):
+    current_time = time.time()
+    file_mtime = os.path.getmtime(filename)
+    cache_age = current_time - file_mtime
+    return cache_age > CACHE_TIME
 
-    # Extract the HTTP method from the request
-    http_method = message.split()[0]
-    print("HTTP method:", http_method)
+# Hàm lưu ảnh vào cache và ghi lại thời điểm lưu cache
+def save_image_to_cache(url, referer, data, CACHE_DIR):
+    directory_root = os.path.join(CACHE_DIR, referer.replace('/', ''))
+    os.makedirs(directory_root, exist_ok=True)
+    # 
+    url = url.replace('/', '_').replace(':', '_')
+    directory_root_sub = os.path.join(directory_root, url)
+    os.makedirs(directory_root_sub, exist_ok=True)
+    # 
+    filename = url
+    cache_time_filename = filename + '.time'
+    # 
+    path_to_filename = os.path.join(directory_root_sub, filename)
+    path_to_filename_cache = os.path.join(directory_root_sub, cache_time_filename)
+    with open(path_to_filename, 'wb') as f:
+        f.write(data)
+    with open(path_to_filename_cache, 'w') as f_time:
+        f_time.write(str(time.time()))
 
-    # Extract the requested filename from the HTTP request
-    filename = message.split()[1].partition("/")[2]
-    print("Requested filename:", filename)
-
-    # Check if the requested website is whitelisted
-    host = filename.split("/")[0]
-
-    if not is_within_allowed_time():
-        # response = response403()
-        response = read_response403("custom403.html")
-    elif not is_whitelisted(host):
-        # response = response403() 
-        response = read_response403("custom403.html")
-    elif http_method not in ["GET", "POST", "HEAD"]:
-        # Return 405 Method Not Allowed with custom HTML content
-        response = b"HTTP/1.0 405 Method Not Allowed\r\nContent-Type: text/html\r\n\r\n"
-        response += b"<html><body><h1>405 Method Not Allowed</h1>"
-        response += b"<p>You are not allowed to use this HTTP method.</p>"
-        response += b"</body></html>"
-    else:
-        # Check if the requested image is in the cache and not expired
-        image_path = os.path.join(cache_directory, filename)
-        if http_method == "GET" and os.path.exists(image_path):
-            current_time = time.time()
-            if filename in last_access_time and current_time - last_access_time[filename] < cache_time:
-                # Serve the image from the cache
-                with open(image_path, "rb") as image_file:
-                    image_data = image_file.read()
-
-                response = b"HTTP/1.0 200 OK\r\nContent-Type: image/jpeg\r\n\r\n" + image_data
-                print("Read from cache:", filename)
-            else:
-                # The image is expired in the cache, fetch it from the web server
-                response = fetch_from_web_server(message, filename)
-
-                # Update the cache with the new image
-                if response.startswith(b"HTTP/1.0 200 OK"):
-                    with open(image_path, "wb") as image_file:
-                        image_file.write(response)
-
-                last_access_time[filename] = current_time
+# Hàm lấy dữ liệu ảnh từ cache và kiểm tra thời gian cache
+def get_image_from_cache(url, referer, CACHE_DIR, CACHE_TIME):
+    directory_root = os.path.join(CACHE_DIR, referer.replace('/', ''))
+    # 
+    url = url.replace('/', '_').replace(':', '_')
+    directory_root_sub = os.path.join(directory_root, url)
+    # 
+    filename = url
+    cache_time_filename = filename + '.time'
+    # 
+    path_to_filename = os.path.join(directory_root_sub, filename)
+    path_to_filename_cache = os.path.join(directory_root_sub, cache_time_filename)
+    if os.path.exists(path_to_filename) and os.path.exists(path_to_filename_cache):
+        if not is_cache_expired(path_to_filename_cache, CACHE_TIME):
+            with open(path_to_filename, 'rb') as f:
+                return f.read()
         else:
-            # Fetch the image from the web server
-            response = fetch_from_web_server(message, filename)
+            os.rmdir(directory_root)
+    return None
 
-            # Cache the image if it is a successful response
-            if response.startswith(b"HTTP/1.0 200 OK"):
-                with open(image_path, "wb") as image_file:
-                    image_file.write(response)
+# Luồng thực hiện việc loại bỏ các đối tượng đã hết hạn khỏi cache
+def remove_expired_cache(CACHE_DIR, CACHE_TIME):
+    while True:
+        current_time = time.time()
+        for root, dirs, files in os.walk(CACHE_DIR):
+            for filename in files:
+                if filename.endswith('.time'):
+                    cache_time_filename = os.path.join(root, filename)
+                    with open(cache_time_filename, 'r') as f_time:
+                        cache_time = float(f_time.read())
+                    cache_age = current_time - cache_time
+                    if cache_age > CACHE_TIME:
+                        parent_directory = os.path.dirname(root)
+                        shutil.rmtree(parent_directory) 
+        time.sleep(CACHE_TIME)
 
-                last_access_time[filename] = time.time()
+# Biến lưu trữ logs truy cập
+access_logs = {}
 
-    # Send the response back to the client
-    tcpCliSock.send(response)
+# Kiểm tra xem URL có phải là một hình ảnh không
+def is_image(url):
+    return any(url.endswith(ext) for ext in ['.jpg', '.jpeg', '.png', '.gif', '.bmp'])
 
-    # Close the client socket after processing the request
-    tcpCliSock.close()
-
-# Function to fetch an image from the web server
-def fetch_from_web_server(request, filename):
+# Xử lí chuỗi request
+def handle_request(request_data):
     try:
-        # Create a socket to connect to the web server
-        c = socket(AF_INET, SOCK_STREAM)
-        host = filename.split("/")[0]
-        web_port = 80
-
-        # Connect to the web server on port 80
-        c.connect((host, web_port))
-
-        # Send the HTTP request to the web server
-        c.send(request.encode())
-
-        # Receive the response from the web server
-        response = b""
-        while True:
-            data = c.recv(1024)
-            if not data:
-                break
-            response += data
-
-        # Close the connection to the web server
-        c.close()
-
-        return response
-
+        line = request_data.decode()
+        method = line.split(' ')[0]
+        url = line.split(' ')[1]
+        host_name = url.split('/')[2]
+        return method, url, host_name
     except:
-        # Error handling for failed requests to the web server
-        response = b"HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\n\r\n404 Not Found"
-        return response
+        return None, None, None
+        
+def get_status(server_respone):
+    buf = server_respone.split(b'\r\n')[0]
+    return buf.split(b' ')[1]
 
-if __name__ == "__main__":
-    if len(sys.argv) <= 1:
-        print('Usage : "python ProxyServer.py config_file"\n[config_file : Path to the configuration file]')
-        sys.exit(2)
+def get_connection_close(server_respone):
+    return "connection: close" in server_respone.decode().lower()
 
-    # Read the server IP and port from the config file
-    config_file = sys.argv[1]
-    server_ip, server_port = read_config_file(config_file)
+def get_content_length(headers):
+    line = headers.split(b"\r\n")
+    for l in line:
+        if l.startswith(b"Content-Length:") or l.startswith(b"content-length"):
+            length = int(l.split(b":")[1].strip())
+            return length
+    return 0
 
-    # Create a server socket, bind it to the server IP and port, and start listening
-    tcpSerSock = socket(AF_INET, SOCK_STREAM)
-    tcpSerSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    tcpSerSock.bind((server_ip, server_port))
-    tcpSerSock.listen(5)  # Maximum number of queued connections is set to 5
+def get_referer(headers):
+    line = headers.split(b"\r\n")
+    for l in line:
+        if l.startswith(b"Referer:"):
+            referer = l.split(b":")[2].strip()
+            return referer.decode()
+    
+def get_server_response(host_name, request_data):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.connect((host_name, 80))
+    server_socket.sendall(request_data)
+    # Get the response from the web server
+    server_response = b''
+    while True:
+        response_chunk = server_socket.recv(1024)
+        server_response += response_chunk
+        if b'\r\n\r\n' in server_response:
+            break
 
-    print('Proxy server is ready to receive connections...')
-
-    # Define a function to handle incoming client connections
-    def start_proxy():
+    # Check for HTTP status code 100 Continue
+    while b'HTTP/1.1 100 Continue\r\n' in server_response:
+        # Send the request data again until you receive a non-100 response
+        server_response = b''
+        server_socket.sendall(request_data)
         while True:
-            # Accept a new client connection
-            try:
-                tcpCliSock, addr = tcpSerSock.accept()
-                print('Received a connection from:', addr)
+            response_chunk = server_socket.recv(1024)
+            server_response += response_chunk
+            if b'\r\n\r\n' in server_response:
+                break
 
-                # Create a new thread to handle the client request
-                client_thread = threading.Thread(target=handle_client, args=(tcpCliSock,))
-                client_thread.start()
+    # Get the header of the response
+    header_end = server_response.find(b"\r\n\r\n")
+    headers = server_response[:header_end]
+    if (b'HEAD' in request_data):
+        server_socket.close()
+        return headers
+    
+    
+    # If the response is an error code, return 403 Forbidden
+    if get_status(server_response) in error_codes:
+        return response403()
+    
+    # If the response is "connection: close", get the response until the end of the response (the web server will close eventually)
+    if get_connection_close(headers):
+        while True:
+            data_chunk = server_socket.recv(1024)
+            if data_chunk:
+                server_response += data_chunk
+            else:
+                return server_response
 
-            except KeyboardInterrupt:
-                # If Ctrl + C is pressed, close the proxy server gracefully
-                print("\nProxy server is shutting down...")
-                tcpSerSock.close()
-                sys.exit()
+    # If the response is not "connection: close" and the body part is not empty, get the response by following the content length or chunked encoding
+    else:
+        
+        chunked_encoding = "transfer-encoding: chunked" in headers.decode().lower()
+        content_length = get_content_length(headers)
+        # If the response is not chunked encoding, get the response by content length
+        if not chunked_encoding and content_length > 0:
+            if len(server_response) < header_end + 4 + content_length:
+                length = content_length - (len(server_response) - header_end - 4)
+                while len(server_response) < header_end + content_length + 4:
+                    server_response += server_socket.recv(length)
+        else:  # If the response is chunked encoding, get the response until meet '0' in the body part
+            end_check = b'0'
+            chunked_part = server_response.split(b"\r\n\r\n")[1]
+            chunks = chunked_part.split(b"\r\n")
+            if end_check not in chunks:
+                while True:
+                    data_chunk = server_socket.recv(1024)
+                    server_response += data_chunk
+                    data_chunks = data_chunk.split(b"\r\n")
+                    if end_check in data_chunks:
+                        break
 
-    # Start the proxy server in its own thread
-    proxy_thread = threading.Thread(target=start_proxy)
-    proxy_thread.start()
+    server_socket.close()
+    return server_response
+
+def extract_response_content(server_response):
+    header_end = server_response.find(b"\r\n\r\n")
+    return server_response[header_end + 4:]
+
+def proxy_thread(client_socket, config):
+    try:
+        CACHE_DIR, ACCESS_LIMIT, SERVER_IP, SERVER_PORT, CACHE_TIME, WHITELISTING, START_TIME, END_TIME = config
+        request_data = client_socket.recv(4096)
+        method, url, host_name = handle_request(request_data)
+        request_text = request_data.decode()  
+        request_lines = request_text.strip().split('\r\n')
+
+        if len(request_lines) > 0 or request_lines != ['']:
+            print("========================================================================================")
+        print(request_data.decode())
+        referer = get_referer(request_data)
+
+        if method not in ['GET', 'POST', 'HEAD'] or not check_ACCESS_LIMIT(START_TIME, END_TIME) or not is_whitelisted(url, WHITELISTING):
+            client_socket.sendall(response403())
+            client_socket.close()
+            return
+
+        if url.startswith('http://'):
+            url = url[7:]
+        elif url.startswith('https://'):
+            url = url[8:]
+        if url.endswith('/'):
+            url = url[:-1]
+
+        response = get_server_response(host_name, request_data)
+        
+        if is_image(url):
+            cached_data = get_image_from_cache(url, referer, CACHE_DIR, CACHE_TIME)
+            if cached_data:
+                header = b'HTTP/1.1 200 OK\r\nCache-Control: no-store\r\n\r\n'
+                client_socket.sendall(header)
+                client_socket.sendall(cached_data)
+            elif is_image(url) and get_status(response) == b'200':
+                save_image_to_cache(url, referer, extract_response_content(response), CACHE_DIR)
+            client_socket.close()
+            return
+        client_socket.sendall(response)
+        client_socket.close()
+        
+    except OSError:
+        client_socket.close()
+
+def main(config_file):
+    config = read_config(config_file)
+    CACHE_DIR, ACCESS_LIMIT, SERVER_IP, SERVER_PORT, CACHE_TIME, WHITELISTING, START_TIME, END_TIME = config
+    if not os.path.exists(CACHE_DIR):
+        os.makedirs(CACHE_DIR)
+
+    # Bắt đầu luồng loại bỏ cache hết hạn
+    remove_cache_thread = threading.Thread(target=remove_expired_cache, args=(CACHE_DIR, CACHE_TIME))
+    remove_cache_thread.daemon = True
+    remove_cache_thread.start()
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((SERVER_IP, SERVER_PORT))
+    server.listen(ACCESS_LIMIT)
+
+    print('Proxy server ' + SERVER_IP + ' is listening on port ' + str(SERVER_PORT) + '...')
+    
+    while True:
+        client_socket, addr = server.accept()
+        print(f"Accepted connection from {addr}")
+        client_thread = threading.Thread(target=proxy_thread, args=(client_socket, config))
+        client_thread.start()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Proxy Server with Config File')
+    parser.add_argument('config', help='Path to the config file')
+    args = parser.parse_args()
+    main(args.config)
